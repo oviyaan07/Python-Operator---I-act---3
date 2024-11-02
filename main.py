d66ca588eb01aad1d3ffadabcdd7e605da47d6eb
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-02 12:27:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+hindi = int(input("enter hindi marks :"))
+english = int(input("enter english marks :"))
+maths = int(input("enter maths marks :"))
+sum=hindi+english+maths
+print ("total=",sum)
+per=(sum/150)*100
+print ("your percentage",per)
